6a63d242c52d6cf739f0a648057e5e321f16b5e3
Yaroslav Senkiv <slavko.senkiv@gmail.com>
2024-08-25 22:09:47 +0200

    123

---
+++ pages/index.js
@@ -0,0 +1,33 @@
+import MeetupList from '../components/meetups/MeetupList';
+
+const DUMMY_MEETUPS = [
+  {
+    id: 'm1',
+    title: 'A first Meetup',
+    image:
+      'https://tourismmedia.italia.it/is/image/mitur/20220128114511-roma-panoramica-roma-lazio-shutterstock-1922377871-cover-1?wid=1920&hei=1320&fit=constrain,1&fmt=webp',
+    address: 'some address here 123 123 123',
+    description: '1st',
+  },
+  {
+    id: 'm2',
+    title: 'A 2d Meetup',
+    image:
+      'https://tourismmedia.italia.it/is/image/mitur/20220128114511-roma-panoramica-roma-lazio-shutterstock-1922377871-cover-1?wid=1920&hei=1320&fit=constrain,1&fmt=webp',
+    address: 'some address here 123 123 123',
+    description: '2d',
+  },
+  {
+    id: 'm3',
+    title: 'A 3d Meetup',
+    image:
+      'https://tourismmedia.italia.it/is/image/mitur/20220128114511-roma-panoramica-roma-lazio-shutterstock-1922377871-cover-1?wid=1920&hei=1320&fit=constrain,1&fmt=webp',
+    address: 'some address here 123 123 123',
+    description: '23d',
+  },
+];
+export default function HomePage() {
+  return (
+      <MeetupList meetups={DUMMY_MEETUPS} />
+  );
+}

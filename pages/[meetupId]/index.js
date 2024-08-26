34f69f553712f8e8a52eaeae3c39989b5c943759
Yaroslav Senkiv <slavko.senkiv@gmail.com>
2024-08-26 09:20:52 +0200

    123

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,12 @@
+import MeetupDetail from '../../pages/[meetupId]';
+
+export default function MeetupDetail(props) {
+  return (
+    <MeetupDetail
+      image="https://tourismmedia.italia.it/is/image/mitur/20220128114511-roma-panoramica-roma-lazio-shutterstock-1922377871-cover-1?wid=1920&hei=1320&fit=constrain,1&fmt=webp"
+      title="A first meetup"
+      address="Some street 5, Some City"
+      description="Some meetup description"
+    />
+  );
+}

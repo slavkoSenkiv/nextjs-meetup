23aa1bb42b0c363056dbc1453f3e4460cfb5df49
Yaroslav Senkiv <slavko.senkiv@gmail.com>
2024-08-31 08:45:38 +0200

    add mongodb data storing

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,7 +1,25 @@
+import { useRouter } from 'next/router';
 import NewMeetupForm from '../../components/meetups/NewMeetupForm';
+
 export default function NewMeetupPage() {
-  function addMeetupHandler(enteredMeetupData) {
+  const router = useRouter()
+
+  async function addMeetupHandler(enteredMeetupData) {
+
+    const responce = await fetch('/api/new-meetup', {
+      method: 'POST',
+      body: JSON.stringify(enteredMeetupData),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+
+    const data = await responce.json();
+
     console.log(enteredMeetupData);
+
+    router.push('/')
+    //router.replace('/') does the same but in a way that user can't go back with back button
     
   }
   return <NewMeetupForm onAddMeetup={addMeetupHandler}/>
